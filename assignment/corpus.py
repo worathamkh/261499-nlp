24c689b5cbf79e96ee33c03292c882496a7ecc74
Woratham Khangtragool <worathamkh@gmail.com>
2018-03-27 11:56:49 +0700

    use deepcut

--- assignment/corpus.py
+++ assignment/corpus.py
@@ -1,11 +1,12 @@
-from pythainlp.tokenize import word_tokenize
+from pythainlp.tokenize import word_tokenize, dict_word_tokenize
 
 tokens = []
 
 with open('corpus/source_list.txt', 'r') as src_list:
     for src in src_list:
         with open('corpus/sources/' + src.rstrip(), 'r') as f:
-            text = ' '.join([x.decode('utf-8') for x in f.readlines()])
-            tokens += [x.strip() for x in word_tokenize(text) if x.strip() != '']
+            text = ' '.join([x.decode('utf-8').lower() for x in f.readlines()])
+            tokens += [x.strip().replace(' ', '') for x in word_tokenize(text, engine='deepcut') if x.strip().replace(' ', '') != '']
+            #  tokens += [x.replace(' ', '') for x in dict_word_tokenize(text, "dict.txt") if x.strip() != '']
 
 print(' '.join(tokens).encode('utf-8'))
